205742f47fc60d774ab5d01bc19b97883b2cfbc5
uffe <uffe@crosser.io>
2021-03-03 13:15:15 +0100

    Sample server

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using XSockets.Core.Common.Socket;
+using XSockets.Plugin.Framework;
+
+namespace SampleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var container = Composable.GetExport<IXSocketServerContainer>())
+            {
+                container.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+}
